c9a9821e9fe0e73da249c9b091806d8c8b7ec883
Bruno Alla <bruno.alla@festicket.com>
2021-01-25 21:19:26 +0000

    Initial commit

---
+++ browniebroke/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from browniebroke.users.models import User
+from browniebroke.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
